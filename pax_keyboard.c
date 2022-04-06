afd4c18c82ca0caf3c6ef74fcd7963cab9141ca9
RobotMan2412 <julian@scheffers.net>
2022-04-06 13:25:38 +0200

    Almost feature complete!

--- pax_keyboard.c
+++ pax_keyboard.c
@@ -23,42 +23,63 @@
 */
 
 #include <pax_keyboard.h>
+#include <esp_timer.h>
 #include <string.h>
 #include <malloc.h>
 
-char *uppercase_board[] = {
+const char *uppercase_board[] = {
 	"QWERTYUIOP",
 	"ASDFGHJKL",
 	" ZXCVBNM ",
-	",", "."
+	" <     > ",
+	"<", ">",
 };
-char *lowercase_board[] = {
+const char *lowercase_board[] = {
 	"qwertyuiop",
 	"asdfghjkl",
 	" zxcvbnm ",
-	",", "."
+	" ,     . ",
+	",", ".",
 };
-char *number_board[] = {
+const char *number_board[] = {
 	"1234567890",
 	"@#$_&-+()/",
 	" *\"';:!? ",
-	"<", ">"
+	" =      \\ ",
+	"=", "\\",
+};
+const char *symbols_board[] = {
+	"()[]{}`~",
+	"/|\\+-_=",
+	" ^%<>'\" ",
+	" ,     . ",
+	",", ".",
+};
+const char **boards[] = {
+	lowercase_board, uppercase_board,
+	number_board,    symbols_board,
 };
 
 static void pkb_char(pax_buf_t *buf, pkb_ctx_t *ctx, float x, float y, float dx, char *text, bool selected) {
-	pax_col_t col = 0xffff0000;
+	pax_vec1_t dims = pax_text_size(ctx->kb_font, ctx->kb_font_size, text);
 	
-	pax_vec1_t dims = pax_text_size(ctx->font, ctx->font_size, text);
-	pax_draw_text(buf, col, ctx->font, ctx->font_size, x-dims.x*0.5, y, text);
-	
-	// Outline?
-	if (selected) {
-		pax_apply_2d(buf, matrix_2d_translate(-dx/2, 0));
-		pax_draw_line(buf, col, x, y, x + dx, y);
-		pax_draw_line(buf, col, x, y+ctx->font_size, x + dx, y+ctx->font_size);
-		pax_draw_line(buf, col, x + dx, y, x + dx, y+ctx->font_size);
-		pax_draw_line(buf, col, x, y, x, y+ctx->font_size);
-		pax_apply_2d(buf, matrix_2d_translate(dx/2, 0));
+	if (selected && ctx->held == PKB_CHARSELECT) {
+		// Infilll!
+		pax_draw_rect(buf, ctx->sel_col, x-dx/2, y, dx, ctx->kb_font_size);
+		pax_draw_text(buf, ctx->sel_text_col, ctx->kb_font, ctx->kb_font_size, x-dims.x*0.5, y, text);
+	} else {
+		pax_draw_text(buf, ctx->text_col, ctx->kb_font, ctx->kb_font_size, x-dims.x*0.5, y, text);
+		
+		// Outline?
+		if (selected) {
+			pax_push_2d(buf);
+			pax_apply_2d(buf, matrix_2d_translate(-dx/2, 0));
+			pax_draw_line(buf, ctx->sel_col, x, y, x + dx - 1, y);
+			pax_draw_line(buf, ctx->sel_col, x, y+ctx->kb_font_size-1, x + dx - 1, y+ctx->kb_font_size-1);
+			pax_draw_line(buf, ctx->sel_col, x + dx - 1, y, x + dx - 1, y+ctx->kb_font_size-1);
+			pax_draw_line(buf, ctx->sel_col, x, y, x, y+ctx->kb_font_size-1);
+			pax_pop_2d(buf);
+		}
 	}
 }
 
@@ -70,41 +91,229 @@ static void pkb_row(pax_buf_t *buf, pkb_ctx_t *ctx, char *row, int selected, flo
 	
 	// Show all of them.
 	for (int i = 0; i < len; i++) {
-		// Draw a TEXT.
+		// Draw a KEY.
 		*tmp = row[i];
 		pkb_char(buf, ctx, x, y, dx, tmp, selected == i);
 		x += dx;
 	}
 }
 
-// Redraw the complete on-screen keyboard.
-void pkb_render(pax_buf_t *buf, pkb_ctx_t *ctx) {
-	pax_col_t text_col = 0xffff0000;
+static void pkb_row_key(pax_buf_t *buf, pkb_ctx_t *ctx, char *row, bool selected, float dx, float y, int keyno) {
+	// Calculate some stuff.
+	size_t len    = strlen(row);
+	int    x      = (buf->width - len * dx + dx) / 2 + dx * keyno;
+	char   tmp[2] = {0,0};
+	
+	// Show one of them.
+	*tmp = row[keyno];
+	pax_draw_rect(buf, ctx->bg_col, x-dx/2, y, dx, ctx->kb_font_size);
+	pkb_char(buf, ctx, x, y, dx, tmp, selected);
+}
+
+// Draw just the text part.
+static void pkb_render_keyb(pax_buf_t *buf, pkb_ctx_t *ctx, bool do_bg) {
+	// Draw background.
+	if (do_bg) {
+		pax_draw_rect(buf, ctx->bg_col, 0, ctx->height-ctx->kb_font_size*4, ctx->width, ctx->kb_font_size*4);
+	}
 	
 	// Select the board to display.
-	char **board = lowercase_board;
-	float  dx    = ctx->width / strlen(*board);
-	float  y     = ctx->y + ctx->height - ctx->font_size * 4;
+	char **board = boards[ctx->board_sel];
+	float  dx    = ctx->width / 10;
+	float  y     = ctx->y + ctx->height - ctx->kb_font_size * 4;
+	
+	// Draw the first three rows.
 	for (int i = 0; i < 3; i ++) {
 		int sel = -1;
 		if (i == ctx->key_y) {
-			ctx->key_x %= strlen(board[i]);
 			sel = ctx->key_x;
 		}
 		pkb_row(buf, ctx, board[i], sel, dx, y);
-		y += ctx->font_size;
+		y += ctx->kb_font_size;
 	}
 	
 	// Spacebar row time.
+	bool space_sel = ctx->key_y == 3 && ctx->key_x > 1 && ctx->key_x < 7;
 	float x = (buf->width - 6 * dx) / 2;
-	pkb_char(buf, ctx, x, y, dx, board[3], false);
-	x += 1.0 * dx;
-	pax_draw_rect(buf, text_col, x, y + ctx->font_size/3, dx*4, ctx->font_size/3);
-	x += 5.0 * dx;
-	pkb_char(buf, ctx, x, y, dx, board[4], false);
 	
-	// Crappy text.
-	pax_draw_text(buf, text_col, NULL, 18, 0, 0, ctx->content);
+	// Left char.
+	pkb_char(buf, ctx, x, y, dx, board[4], ctx->key_y == 3 && ctx->key_x == 1);
+	x += 1.0 * dx;
+	
+	// SPACE.
+	if (space_sel && ctx->held == PKB_CHARSELECT) {
+		pax_draw_rect(buf, ctx->sel_col, x-dx/2, y, dx*5, ctx->kb_font_size);
+		pax_draw_rect(buf, ctx->sel_text_col, x, y + ctx->kb_font_size/3, dx*4, ctx->kb_font_size/3);
+	} else {
+		pax_draw_rect(buf, ctx->text_col, x, y + ctx->kb_font_size/3, dx*4, ctx->kb_font_size/3);
+		if (space_sel) {
+			// Outline rect?
+			pax_push_2d(buf);
+			pax_apply_2d(buf, matrix_2d_translate(-dx/2, 0));
+			pax_draw_line(buf, ctx->sel_col, x, y, x + dx*5 - 1, y);
+			pax_draw_line(buf, ctx->sel_col, x, y+ctx->kb_font_size-1, x + dx*5 - 1, y+ctx->kb_font_size-1);
+			pax_draw_line(buf, ctx->sel_col, x + dx*5 - 1, y, x + dx*5 - 1, y+ctx->kb_font_size-1);
+			pax_draw_line(buf, ctx->sel_col, x, y, x, y+ctx->kb_font_size-1);
+			pax_pop_2d(buf);
+		}
+	}
+	
+	// Right char.
+	x += 5.0 * dx;
+	pkb_char(buf, ctx, x, y, dx, board[5], ctx->key_y == 3 && ctx->key_x == 7);
+	
+}
+
+// Draw just the board part.
+static void pkb_render_text(pax_buf_t *buf, pkb_ctx_t *ctx, bool do_bg) {
+	// Draw background.
+	if (do_bg) {
+		pax_draw_rect(buf, ctx->bg_col, 0, 0, ctx->width, ctx->height - ctx->kb_font_size*4);
+	}
+	if (ctx->key_y == -1) {
+		// Outline us.
+		pax_outline_rect(buf, ctx->sel_col, 0, 0, ctx->width, ctx->height - ctx->kb_font_size*4);
+	}
+	
+	// Some setup.
+	float x = 2;
+	float y = 2;
+	char tmp[2] = {0, 0};
+	
+	// Draw everything.
+	for (int i = 0; i < strlen(ctx->content); i++) {
+		if (ctx->cursor == i) {
+			// The cursor in between the input.
+			pax_draw_line(buf, ctx->sel_col, x, y, x, y + ctx->text_font_size - 1);
+		}
+		
+		// The character of the input.
+		tmp[0] = ctx->content[i];
+		pax_vec1_t dims = pax_text_size(ctx->text_font, ctx->text_font_size, tmp);
+		
+		if (x + dims.x > ctx->width - 2) {
+			// Word wrap.
+			x  = 2;
+			y += ctx->text_font_size;
+		}
+		pax_draw_text(buf, ctx->text_col, ctx->text_font, ctx->text_font_size, x, y, tmp);
+		x += dims.x;
+	}
+	if (ctx->cursor == strlen(ctx->content)) {
+		// The cursor after the input.
+		pax_draw_line(buf, ctx->sel_col, x, y, x, y + ctx->text_font_size - 1);
+	}
+}
+
+// Draw one specific key.
+static void pkb_render_key(pax_buf_t *buf, pkb_ctx_t *ctx, int key_x, int key_y) {
+	if (key_y == -1) {
+		pkb_render_text(buf, ctx, true);
+		return;
+	}
+	
+	// Select the board to display.
+	char **board = boards[ctx->board_sel];
+	float  dx    = ctx->width / 10;
+	float  y     = ctx->y + ctx->height - ctx->kb_font_size * 4;
+	
+	if (key_y < 3) {
+		// Draw one of the first three rows.
+		y += ctx->kb_font_size * key_y;
+		pkb_row_key(buf, ctx, board[key_y], key_y == ctx->key_y && key_x == ctx->key_x, dx, y, key_x);
+		y += ctx->kb_font_size;
+	} else {
+		// Spacebar row time.
+		y += ctx->kb_font_size * 3;
+		bool space_sel = ctx->key_y == 3 && ctx->key_x > 1 && ctx->key_x < 7;
+		float x = (buf->width - 6 * dx) / 2;
+		
+		// Left char.
+		if (key_x == 1) {
+			pax_draw_rect(buf, ctx->bg_col, x-dx/2, y, dx, ctx->kb_font_size);
+			pkb_char(buf, ctx, x, y, dx, board[4], ctx->key_y == 3 && ctx->key_x == 1);
+		}
+		x += 1.0 * dx;
+		
+		// SPACE.
+		if (ctx->held == PKB_CHARSELECT) {
+			pax_draw_rect(buf, ctx->sel_col, x-dx/2, y, dx*5, ctx->kb_font_size);
+			pax_draw_rect(buf, ctx->sel_text_col, x, y + ctx->kb_font_size/3, dx*4, ctx->kb_font_size/3);
+		} else {
+			pax_draw_rect(buf, ctx->bg_col, x-dx/2, y, dx*5, ctx->kb_font_size);
+			pax_draw_rect(buf, ctx->text_col, x, y + ctx->kb_font_size/3, dx*4, ctx->kb_font_size/3);
+			if (space_sel) {
+				// Outline rect?
+				pax_push_2d(buf);
+				pax_apply_2d(buf, matrix_2d_translate(-dx/2, 0));
+				pax_draw_line(buf, ctx->sel_col, x, y, x + dx*5 - 1, y);
+				pax_draw_line(buf, ctx->sel_col, x, y+ctx->kb_font_size-1, x + dx*5 - 1, y+ctx->kb_font_size-1);
+				pax_draw_line(buf, ctx->sel_col, x + dx*5 - 1, y, x + dx*5 - 1, y+ctx->kb_font_size-1);
+				pax_draw_line(buf, ctx->sel_col, x, y, x, y+ctx->kb_font_size-1);
+				pax_pop_2d(buf);
+			}
+		}
+		
+		// Right char.
+		x += 5.0 * dx;
+		if (key_x == 7) {
+			pax_draw_rect(buf, ctx->bg_col, x-dx/2, y, dx, ctx->kb_font_size);
+			pkb_char(buf, ctx, x, y, dx, board[5], ctx->key_y == 3 && ctx->key_x == 7);
+		}
+	}
+}
+
+// Redraw the complete on-screen keyboard.
+void pkb_render(pax_buf_t *buf, pkb_ctx_t *ctx) {
+	pax_background(buf, ctx->bg_col);
+	
+	// Draw the board.
+	pkb_render_keyb(buf, ctx, false);
+	// Time to draw some text.
+	pkb_render_text(buf, ctx, false);
+	
+	// Mark as not dirty.
+	ctx->dirty      = false;
+	ctx->kb_dirty   = false;
+	ctx->sel_dirty  = false;
+	ctx->text_dirty = false;
+	ctx->last_key_x = ctx->key_x;
+	ctx->last_key_y = ctx->key_y;
+}
+
+// Redraw only the changed parts of the on-screen keyboard.
+void pkb_redraw(pax_buf_t *buf, pkb_ctx_t *ctx) {
+	if (ctx->text_dirty) {
+		pkb_render_text(buf, ctx, true);
+	}
+	if (ctx->kb_dirty) {
+		pkb_render_keyb(buf, ctx, true);
+	} else if (ctx->sel_dirty) {
+		pkb_render_key(buf, ctx, ctx->last_key_x, ctx->last_key_y);
+		pkb_render_key(buf, ctx, ctx->key_x,      ctx->key_y);
+	}
+	
+	// Mark as not dirty.
+	ctx->dirty      = false;
+	ctx->kb_dirty   = false;
+	ctx->sel_dirty  = false;
+	ctx->text_dirty = false;
+	ctx->last_key_x = ctx->key_x;
+	ctx->last_key_y = ctx->key_y;
+}
+
+// The loop that allows input repeating.
+void pkb_loop(pkb_ctx_t *ctx) {
+	int64_t now = esp_timer_get_time();
+	if (!ctx->held) return;
+	bool is_dir = (ctx->held >= PKB_UP) && (ctx->held <= PKB_RIGHT);
+	
+	if ((ctx->hold_start + 1000000 < now) || (is_dir && ctx->hold_start + 250000 < now)) {
+		// 8 repeats per second.
+		if (ctx->last_press + 125000 < now) {
+			pkb_press(ctx, ctx->held);
+		}
+	}
 }
 
 static void pkb_delete(pkb_ctx_t *ctx, bool is_backspace) {
@@ -132,6 +341,7 @@ static void pkb_delete(pkb_ctx_t *ctx, bool is_backspace) {
 		ctx->content_cap = oldlen;
 		ctx->content = realloc(ctx->content, ctx->content_cap);
 	}
+	ctx->text_dirty = true;
 }
 
 static void pkb_append(pkb_ctx_t *ctx, char value) {
@@ -151,56 +361,79 @@ static void pkb_append(pkb_ctx_t *ctx, char value) {
 	// And finally insert at the character.
 	ctx->content[ctx->cursor] = value;
 	ctx->cursor ++;
+	ctx->text_dirty = true;
 }
 
 // A pressing of the input.
 void pkb_press(pkb_ctx_t *ctx, pkb_input_t input) {
-	char **board = lowercase_board;
-	ctx->held = input;
+	char **board = boards[ctx->board_sel];
+	ctx->last_press = esp_timer_get_time();
 	switch (input) {
 			// Cursor movements.
 			size_t rowlen;
 		case PKB_UP:
 			ctx->key_y --;
-			if (ctx->key_y < 0) ctx->key_y = 3;
-			rowlen = strlen(board[ctx->key_y]);
-			if (ctx->key_x >= rowlen) ctx->key_x = rowlen - 1;
+			if (ctx->key_y < -1) ctx->key_y = 3;
+			else if (ctx->key_y != -1) {
+				rowlen = strlen(board[ctx->key_y]);
+				if (ctx->key_x >= rowlen) ctx->key_x = rowlen - 1;
+			}
+			ctx->sel_dirty = true;
 			break;
+			
 		case PKB_DOWN:
 			ctx->key_y ++;
 			ctx->key_y %= 4;
 			rowlen = strlen(board[ctx->key_y]);
 			if (ctx->key_x >= rowlen) ctx->key_x = rowlen - 1;
+			ctx->sel_dirty = true;
 			break;
+			
 		case PKB_LEFT:
-			ctx->key_x --;
-			if (ctx->key_x < 0) ctx->key_x = strlen(board[ctx->key_y]) - 1;
+			if (ctx->key_y == -1) {
+				if (ctx->cursor > 0) ctx->cursor --;
+			} else if (ctx->key_y == 3 && ctx->key_x > 1 && ctx->key_x < 7) {
+				ctx->key_x = 1;
+			} else {
+				ctx->key_x --;
+				if (ctx->key_x < 0) ctx->key_x = strlen(board[ctx->key_y]) - 1;
+			}
+			ctx->sel_dirty = true;
 			break;
+			
 		case PKB_RIGHT:
-			ctx->key_x ++;
-			ctx->key_x %= strlen(board[ctx->key_y]);
+			if (ctx->key_y == -1) {
+				if (ctx->cursor < strlen(ctx->content)) ctx->cursor ++;
+			} else if (ctx->key_y == 3 && ctx->key_x > 1 && ctx->key_x < 7) {
+				ctx->key_x = 7;
+			} else {
+				ctx->key_x ++;
+				ctx->key_x %= strlen(board[ctx->key_y]);
+			}
+			ctx->sel_dirty = true;
 			break;
+			
 			// Enter a character.
 		case PKB_CHARSELECT:
+			ctx->sel_dirty |= ctx->held != PKB_CHARSELECT;
 			if (ctx->key_y == 3) {
 				switch (ctx->key_x) {
-					default:
 					case 0:
 						// Something?
 						break;
 					case 1:
 						// Magic.
-						pkb_append(ctx, *board[3]);
+						pkb_append(ctx, *board[4]);
 						break;
-					case 2:
+					default:
 						// Spacebar.
 						pkb_append(ctx, ' ');
 						break;
-					case 3:
+					case 7:
 						// mAGIC.
-						pkb_append(ctx, *board[4]);
+						pkb_append(ctx, *board[5]);
 						break;
-					case 4:
+					case 8:
 						// Enter idk.
 						break;
 				}
@@ -219,14 +452,61 @@ void pkb_press(pkb_ctx_t *ctx, pkb_input_t input) {
 				pkb_append(ctx, board[ctx->key_y][ctx->key_x]);
 			}
 			break;
+			
+			// Shift key, the pressening.
+		case PKB_SHIFT:
+			ctx->board_sel |= 1;
+			ctx->kb_dirty = true;
+			break;
+			
+			// Next keyboard.
+		case PKB_MODESELECT:
+			ctx->board_sel ++;
+			ctx->board_sel %= 4;
+			rowlen = strlen(board[ctx->key_y]);
+			if (ctx->key_x >= rowlen) ctx->key_x = rowlen - 1;
+			ctx->kb_dirty = true;
+			break;
+			
+			// Backspace.
+		case PKB_DELETE_BEFORE:
+			pkb_delete(ctx, true);
+			break;
+			
+			// Delete.
+		case PKB_DELETE_AFTER:
+			pkb_delete(ctx, false);
+			break;
 		default:
 			break;
 	}
+	if (input != PKB_SHIFT && input != ctx->held) {
+		ctx->held = input;
+		ctx->hold_start = esp_timer_get_time();
+	}
+	ctx->dirty = true;
 }
 
 // A relealing of the input.
 void pkb_release(pkb_ctx_t *ctx, pkb_input_t input) {
+	switch (input) {
+			// Shift key, the releasening.
+		case PKB_SHIFT:
+			ctx->dirty = true;
+			ctx->board_sel &= ~1;
+			ctx->kb_dirty = true;
+			break;
+			
+			// Unpress them char.
+		case PKB_CHARSELECT:
+			ctx->sel_dirty = true;
+			break;
+			
+		default:
+			break;
+	}
 	if (ctx->held == input) {
 		ctx->held = PKB_NO_INPUT;
+		ctx->dirty = true;
 	}
 }
--- pax_keyboard.h
+++ pax_keyboard.h
@@ -26,6 +26,8 @@
 #define PAX_KEYBOARD_H
 
 #include <pax_gfx.h>
+#include <pax_shapes.h>
+#include <pax_fonts.h>
 
 // A number of inputs supported by the PAX keyboard.
 typedef enum {
@@ -41,43 +43,94 @@ typedef enum {
 	PKB_MODESELECT,
 	// Enter a character.
 	PKB_CHARSELECT,  
+	// The same thing as the shift key.
+	// Goes between PKB_LOWERCASE and PKB_UPPERCASE or PKB_NUMBERS and PKB_SYMBOLS.
+	PKB_SHIFT,
 } pkb_input_t;
+
+// The type of keyboard currently selected.
+typedef enum {
+	// Lowercase and .,
+	PKB_LOWERCASE,
+	// Uppercase and <>
+	PBK_UPPERCASE,
+	// Numbers and symbols 1/2
+	PKB_NUMBERS,
+	// Symbols 2/2
+	PKB_SYMBOLS,
+} pkb_keyboard_t;
 
 // The PAX keyboard context used for drawing and alike.
 typedef struct {
 	// Position on screen of the keyboard.
-	int    x, y;
+	int            x, y;
 	// Maximum size of the keyboard.
-	int    width, height;
+	int            width, height;
 	
 	// Content of the keyboard.
-	char  *content;
+	char          *content;
 	// Size in bytes of capacity of the content buffer.
-	size_t content_cap;
+	size_t         content_cap;
 	
 	// Starting position of the selection in the text box.
-	int    selection;
+	int            selection;
 	// Cursor position of the text box.
-	int    cursor;
+	int            cursor;
 	
 	// Cursor position of the keyboard.
-	int    key_x, key_y;
+	int            key_x, key_y;
 	// The currently held input.
-	pkb_input_t held;
+	pkb_input_t    held;
+	// The time that holding the input started.
+	int64_t        hold_start;
+	// The last time pkb_press was called.
+	int64_t        last_press;
 	
 	// Whether the keyboard is multi-line.
-	bool   multiline;
+	bool           multiline;
 	// Whether the keyboard is in insert mode.
-	bool   insert;
+	bool           insert;
+	// The board that is currently selected.
+	pkb_keyboard_t board_sel;
 	
-	// The font to use.
-	pax_font_t *font;
-	// The font size to use.
-	float       font_size;
+	// The font to use for the keyboard.
+	pax_font_t    *kb_font;
+	// The font size to use for the keyboard.
+	float          kb_font_size;
+	// The font to use for the text.
+	pax_font_t    *text_font;
+	// The font size to use for the text.
+	float          text_font_size;
+	// The text color to use.
+	pax_col_t      text_col;
+	// The text color to use when a character is being held down.
+	pax_col_t      sel_text_col;
+	// The selection color to use.
+	pax_col_t      sel_col;
+	// The background color to use.
+	pax_col_t      bg_col;
+	
+	// Whether something has changed since last draw.
+	bool           dirty;
+	// Whether the text has changed since last draw.
+	bool           text_dirty;
+	// Whether the keyboard has changed since last draw.
+	bool           kb_dirty;
+	// Whether just the selected character has changed since last draw.
+	bool           sel_dirty;
+	// Previous cursor position of the keyboard.
+	// Used for sel_dirty.
+	int            last_key_x, last_key_y;
 } pkb_ctx_t;
 
 // Redraw the complete on-screen keyboard.
 void pkb_render (pax_buf_t *buf, pkb_ctx_t *ctx);
+// Redraw only the changed parts of the on-screen keyboard.
+void pkb_redraw (pax_buf_t *buf, pkb_ctx_t *ctx);
+
+// The loop that allows input repeating.
+void pkb_loop   (pkb_ctx_t *ctx);
+
 // A pressing of the input.
 void pkb_press  (pkb_ctx_t *ctx, pkb_input_t input);
 // A relealing of the input.
